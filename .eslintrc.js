df61d7e8fbf8a9950d65916260a324ddae3faae2
rickecr <richelton14@gmail.com>
2020-02-19 22:15:04 -0300

    #1 - ESLint e Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'no-use-before-define': 'off'
+  },
+};
--- src/SelectMultiple.js
+++ src/SelectMultiple.js
@@ -1,69 +1,76 @@
-import React, { useEffect, useState } from "react";
-import { View } from "react-native";
+import React, {useEffect, useState} from 'react';
+import {View} from 'react-native';
 
-import { CheckBox } from "react-native-elements";
+import {CheckBox} from 'react-native-elements';
 
-const SelectMultiple = (props) => {
-	const [optionsSelect, setOptionsSelect] = useState(props.options);
+const SelectMultiple = props => {
+  const [optionsSelect, setOptionsSelect] = useState([]);
 
-	useEffect(() => {
-		preprocessOption();
-	}, []);
+  useEffect(() => {
+    preprocessOption();
+  }, []);
 
-	function preprocessOption() {
-		const aux = [];
-		props.options.forEach(e => {
-			const op = { label: e.label, value: e.value, checked: false, nomeCategoria: e.nomeCategoria };
-			aux.push(op);
-		});
+  function preprocessOption() {
+    const {options} = props;
 
-		setOptionsSelect(aux);
-	}
+    const aux = [];
+    options.forEach(e => {
+      const op = {
+        label: e.label,
+        value: e.value,
+        checked: false,
+        nomeCategoria: e.nomeCategoria,
+      };
+      aux.push(op);
+    });
 
-	function onSelected(data, item) {
-		const aux = [];
-		data.forEach(element => {
-			if(element.checked){
-				aux.push(element);
-			}
-		});
+    setOptionsSelect(aux);
+  }
 
-		props.onSelected(aux, item);
-	}
+  function onSelected(data, item) {
+    const aux = [];
+    data.forEach(element => {
+      if (element.checked) {
+        aux.push(element);
+      }
+    });
 
-	return (
-		<View>
-			{optionsSelect.length > 0 && <View>
-				{optionsSelect.map(e => {
-					return (
-						<CheckBox
-							containerStyle={{
-								backgroundColor: 'transparent',
-								borderColor: 'transparent',
-							}}
-							key={e.value}
-							title={e.label}
-							checked={e.checked}
-							onPress={() => {
-								const c = [];
-								optionsSelect.forEach(el => {
-									if (e !== el) {
-										c.push(el);
-									} else {
-										e.checked = !e.checked;
-										c.push(e);
-									}
-								});
+    props.onSelected(aux, item);
+  }
 
-								setOptionsSelect(c);
-								onSelected(optionsSelect, e);
-							}}
-							/>)
-				})}
-			</View>
-			}
-		</View>
-	);
-}
+  return (
+    <View>
+      {optionsSelect.length > 0 && (
+        <View>
+          {optionsSelect.map(e => (
+            <CheckBox
+              containerStyle={{
+                backgroundColor: 'transparent',
+                borderColor: 'transparent',
+              }}
+              key={e.value}
+              title={e.label}
+              checked={e.checked}
+              onPress={() => {
+                const c = [];
+                optionsSelect.forEach(el => {
+                  if (e !== el) {
+                    c.push(el);
+                  } else {
+                    e.checked = !e.checked;
+                    c.push(e);
+                  }
+                });
+
+                setOptionsSelect(c);
+                onSelected(optionsSelect, e);
+              }}
+            />
+          ))}
+        </View>
+      )}
+    </View>
+  );
+};
 
 export default SelectMultiple;
